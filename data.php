4d8cca16eddecb667e07e5330b499b13867a3079
hadacchi <hadacchi@users.noreply.github.com>
2017-09-16 23:45:19 +0900

    translate comment into English

--- data.php
+++ data.php
@@ -9,12 +9,12 @@ $pdo = new PDO(
     'mysql:host='.$dbattr['host'].';dbname='.$dbattr['dbname'],
     $dbattr['user'],
     $dbattr['pass']);
-// 1日分=24*60/10=144 records
-// 最新1日の取得 = order by desc + limit 144
+// the number of records for a day is 24*60/10=144 records.
+// to read latest 144 records, order by desc + limit 144
 $sql = $pdo->prepare("SELECT DATE_FORMAT(t, '%Y-%m-%d %H:%i') AS m, AVG(val) AS value FROM ".$dbattr['table']." GROUP BY SUBSTRING(m, 1, 15) ORDER BY m DESC LIMIT 576");
 $sql->execute();
 
-// dygraphsには時系列はascで渡さないといけない
+// dygraphs accept time series csv order by asc, thus reverse records order by desc.
 foreach(array_reverse($sql->fetchAll()) as $row) {
     echo $row['m'], ',', $row['value'], "\n";
 }
